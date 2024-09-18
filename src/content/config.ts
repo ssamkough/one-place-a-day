a0babf41c446fe269bf1436cf52c01323adefddf
Sammy <sammysamkough@hotmail.com>
2024-09-18 01:00:06 -0400

    initial setup

---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { defineCollection, z } from "astro:content";
+
+const placesCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    website_url: z.string().optional(),
+    has_wifi: z.boolean().optional(),
+    scores: z
+      .object({
+        food: z.number(),
+        service: z.number(),
+        aesthetics: z.number(),
+        bathroom: z.number(),
+      })
+      .optional(),
+    location: z.object({
+      country: z.string(),
+    }),
+    thoughts: z.string(),
+    initial_visit_date: z.string(),
+    // created_date: z.date().transform((str) => new Date(str)),
+    // updated_date: z.date().transform((str) => new Date(str)),
+  }),
+});
+export const collections = {
+  places: placesCollection,
+};
---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export const TITLE = "One Place A Day";
+
+export const LIST = [
+  { title: "Home", href: "/" },
+  { title: "Places", href: "places" },
+  { title: "About", href: "about" },
+  { title: "Contact", href: "contact" },
+];
