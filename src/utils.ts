9527bd3702787f694fb8f28b389108d9951bb038
Sammy <sammysamkough@hotmail.com>
2025-08-04 02:43:58 -0400

    quick fixes

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,6 @@ export const TITLE = "One Place A Day";
 
 export const NAVIGATION_LINKS = [
   { title: "Home", href: "/" },
-  { title: "Places", href: "places" },
-  { title: "About", href: "about" },
+  { title: "Places", href: "/places" },
+  { title: "About", href: "/about" },
 ];
